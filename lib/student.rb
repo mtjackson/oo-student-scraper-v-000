169f0600759b6622b2cbdb9d267964aeec1b633f
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-15 01:08:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,12 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+    self.send(:twitter=, student_hash[:twitter])
+    self.send(:linkedin=, student_hash[:linkedin])
+    self.send(:github=, student_hash[:github])
+    self.send(:blog=, student_hash[:blog])
+    self.send(:profile_quote=, student_hash[:profile_quote])
+    self.send(:bio=, student_hash[:bio])
   end
 
   def self.all
